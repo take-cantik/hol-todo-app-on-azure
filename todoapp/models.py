1ed92f898110d857f27af529d3a4332fb750120b
take-cantik <take.cantik17@gmail.com>
2022-05-14 12:17:47 +0900

    Add: Task model and config db

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField('Title', max_length=100, blank=False)
+    created_at = models.DateTimeField('作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField('更新日時',auto_now=True)
+
+    def __str__(self):
+        return self.title
